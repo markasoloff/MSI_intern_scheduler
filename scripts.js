72505b8b7bf4b1eb37f7cf954632753d415c8c63
“Marksoloff” <marksoloff@gmail.com>
2019-04-30 12:52:20 -0500

    test

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+var hi = "hello world";
+document.querySelector("li").innerHTML = hi;
+
+var internName = document.getElementById("name").innerHTML;
+var studentList = localStorage.setItem('savedName', internName);
+
+// if (studentList != " ") {
+
+// }
+console.log(studentList);
+
+// var cat = localStorage.getItem('myCat');
