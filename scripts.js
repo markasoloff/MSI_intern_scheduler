e648aa513603aef5b892c59ccde041572ef951fd
“Marksoloff” <marksoloff@gmail.com>
2019-04-30 14:33:55 -0500

    added bootstrap

--- scripts.js
+++ scripts.js
@@ -1,13 +1,28 @@
 
-var hi = "hello world";
-document.querySelector("li").innerHTML = hi;
+var clicky = document.querySelector('#name').value;
+var button = document.querySelector("btn btn-primary");
+button.addEventListener("click", console.log(clicky.value), false);
 
-var internName = document.getElementById("name").innerHTML;
-var studentList = localStorage.setItem('savedName', internName);
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+});
+
+// button.addEventListener("click", console.log(clicky),false);
+// function wawa() {
+//   clicky.addEventListener("click", console.log(clicky.value), false);
+// }
+
+// var internName = document.getElementByID('name').value;
+// var submitButton = document.getElementByClass('go').value;
+
+// document.getElementById('alert').innerHTML = 'The user input is: ' + variable;
+
+
+// var studentList = localStorage.setItem('savedName', internName);
 
 // if (studentList != " ") {
 
 // }
-console.log(studentList);
+// console.log(document.getIteim(studentList));
 
 // var cat = localStorage.getItem('myCat');
