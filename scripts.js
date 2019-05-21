cab3e61dc363c0b06748a18829168337a3b5c13f
“Marksoloff” <marksoloff@gmail.com>
2019-05-21 13:03:21 -0500

    added localstorage experimentation in js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+// function nameGetter() {  
+//   var inputName = document.getElementByID("name");
+//   localStorage.setItem("name", inputName.value);
+// }
+
+
+
+var storedValue = localStorage.getItem("name");
+
+
 
 var clicky = document.querySelector('#name').value;
 var button = document.querySelector("btn btn-primary");
@@ -7,22 +17,4 @@ window.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM fully loaded and parsed');
 });
 
-// button.addEventListener("click", console.log(clicky),false);
-// function wawa() {
-//   clicky.addEventListener("click", console.log(clicky.value), false);
-// }
 
-// var internName = document.getElementByID('name').value;
-// var submitButton = document.getElementByClass('go').value;
-
-// document.getElementById('alert').innerHTML = 'The user input is: ' + variable;
-
-
-// var studentList = localStorage.setItem('savedName', internName);
-
-// if (studentList != " ") {
-
-// }
-// console.log(document.getIteim(studentList));
-
-// var cat = localStorage.getItem('myCat');
